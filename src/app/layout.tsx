5e4d03bd726e65dd26b8046a7bcb193207d6138e
dok7-software <dok7io.master@gmail.com>
2026-07-08 17:35:39 +0200

    DA: agregar GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import Script from "next/script";
 import { fontVariables } from "@/lib/fonts";
 import "./globals.css";
+
+const GTM_ID = "GTM-NCDC2FS2";
 
 export default function RootLayout({
   children,
@@ -11,7 +14,26 @@ export default function RootLayout({
       data-scroll-behavior="smooth"
       className={`${fontVariables} h-full`}
     >
-      <body className="min-h-full font-clear-sans">{children}</body>
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
+      <body className="min-h-full font-clear-sans">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
